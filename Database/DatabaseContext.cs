a367c381a0088a278144bbcf2faf4a1a9ea7d8e5
NA\SChak103 <SChak103@its.jnj.com>
2022-04-14 12:10:18 +0530

    db conn updated

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -14,8 +14,8 @@ namespace CREDMicroService.Database
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"data source=DESKTOP-8ENM95I\SqlExpress;initial catalog=MicroServiceDB;Integrated Security=SSPI;");
-            //optionsBuilder.UseSqlServer(@"Server=tcp:azsqlserverdatabase.database.windows.net,1433;Initial Catalog=microservicedb;Persist Security Info=False;User ID=azsa;Password=TcsP@ssw0rd@4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //optionsBuilder.UseSqlServer(@"data source=DESKTOP-8ENM95I\SqlExpress;initial catalog=MicroServiceDB;Integrated Security=SSPI;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:azsqlserverdatabase.database.windows.net,1433;Initial Catalog=microservicedb;Persist Security Info=False;User ID=azsa;Password=TcsP@ssw0rd@4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         }
 
